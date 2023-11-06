080a2360e3f07f23db74074d27b87a6ce9c70eb6
Melga10 <gabrielmelgarejo18@gmail.com>
2023-11-06 17:09:11 -0300

    fix-bug

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ console.log("firts commit")
 
 function sayHello ()[
     console.log("Hello")
+<<<<<<< Updated upstream
 ]
+=======
+}
+function sayBye () {
+    console.log("Bye")
+}
+>>>>>>> Stashed changes
